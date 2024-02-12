1e164fd88be97e5790481a3aaca43c5b6b2201d2
DhrubaNandi1256 <drbnandi6961@gmail.com>
2024-02-12 16:47:03 +0600

    firts commit

---
+++ pyexercise/pro/first_code.py
@@ -0,0 +1,8 @@
+print('hlw world1');
+print("hlw", "hi", "bye");
+print(1, 'plus', 2, 'euals', 1+2 )
+print("hello","how r u",sep=",")
+x = ("apple", "banana", "cherry")
+print(x)
+name=input("give me ur name")
+print(name)
